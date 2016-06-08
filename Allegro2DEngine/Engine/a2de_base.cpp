2578feec0f8ca855216893ff1f7ba74640deb665
cugone <cugone@gmail.com>
2016-06-08 16:45:32 -0500

    Added Explicit uninstall calls
    Added explicit calls to al_uninstall_* calls.

--- Allegro2DEngine/Engine/a2de_base.cpp
+++ Allegro2DEngine/Engine/a2de_base.cpp
@@ -5,32 +5,51 @@
 A2DE_BEGIN
 
 bool a2de_init() {
-    std::cout << "Calling a2de_init" << std::endl;
+    std::cout << "Calling a2de_init...";
+    std::cout.flush();
+
     bool system_init_result = al_install_system(ALLEGRO_VERSION_INT, std::atexit);
-    bool image_init_result = al_init_image_addon();
-    bool primitives_init_result = al_init_primitives_addon();
-    al_init_font_addon();
-    bool ttf_init_result = al_init_ttf_addon();
-    bool install_audio_result = al_install_audio();
-    bool acodec_init_result = al_init_acodec_addon();
-    bool native_dialog_init_result = al_init_native_dialog_addon();
-    bool keyboard_result = al_install_keyboard();
-    bool mouse_result = al_install_mouse();
-    bool joystick_result = al_install_joystick();
+    bool system_installed = al_is_system_installed();
+    bool image_init_result = system_installed && al_init_image_addon();
+    bool primitives_init_result = system_installed && al_init_primitives_addon();
+    bool font_init_result = system_installed && al_init_font_addon();
+    bool ttf_init_result = system_installed && al_init_ttf_addon();
+    bool install_audio_result = system_installed && al_install_audio();
+    bool acodec_init_result = system_installed && al_init_acodec_addon();
+    bool native_dialog_init_result = system_installed && al_init_native_dialog_addon();
+    bool install_keyboard_result = system_installed && al_install_keyboard();
+    bool install_mouse_result = system_installed && al_install_mouse();
+    bool install_joystick_result = system_installed && al_install_joystick();
     bool result = system_init_result || image_init_result ||
-        primitives_init_result || ttf_init_result ||
+        primitives_init_result || font_init_result || ttf_init_result ||
         install_audio_result || acodec_init_result ||
-        native_dialog_init_result || keyboard_result ||
-        mouse_result || joystick_result;
+        native_dialog_init_result || install_keyboard_result ||
+        install_mouse_result || install_joystick_result;
 
     std::atexit(a2de::a2de_deinit);
 
+    std::cout << "done" << std::endl;
     return result;
 }
 
 void a2de_deinit() {
-    std::cout << "Calling a2de_deinit" << std::endl;
+    std::cout << "Calling a2de_deinit...";
+    std::cout.flush();
+
     a2de::BitmapCache::_cache.clear();
+
+    al_uninstall_joystick();
+    al_uninstall_mouse();
+    al_uninstall_keyboard();
+    al_shutdown_native_dialog_addon();
+    al_uninstall_audio();
+    al_shutdown_ttf_addon();
+    al_shutdown_font_addon();
+    al_shutdown_primitives_addon();
+    al_shutdown_image_addon();
+    al_uninstall_system();
+    
+    std::cout << "done" << std::endl;
 }
 
 A2DE_END
